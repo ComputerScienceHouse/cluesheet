73ec501ee19160b05c89ab355966435e73d1fa26
Max Meinhold <mxmeinhold@gmail.com>
2025-05-05 19:55:04 -0400

    Add POST clue route
    No support for rules yet

--- backend/main.go
+++ backend/main.go
@@ -209,6 +209,133 @@ func main() {
 		rw.Write(data)
 	})
 
+	// POST /api/v1/cluesheet/{id}/clue
+	// params:
+	// - description
+	// - rules are TBD
+	// - creator
+	// - created and edited are derived/generated
+	// - tags (optional)
+	// - parent_clue_id (optional)
+	v1.Path("/cluesheet/{id}/clue").Methods("POST").HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := uuid.Parse(vars["id"])
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("failed to parse uuid '%s': '%s'", vars["id"], err), 400)
+			return
+		}
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("failed to read request: %s", err.Error()), 400)
+			return
+		}
+
+		var params struct {
+			Description  string
+			Creator      string
+			Tags         []string
+			ParentClueId *uuid.UUID `json:"parent_clue_id"`
+		}
+
+		err = json.Unmarshal(body, &params)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("failed to parse request: %s", err.Error()), 400)
+			return
+		}
+
+		if params.Description == "" {
+			http.Error(rw, "Description must not be empty", 400)
+			return
+		}
+
+		if params.Creator == "" {
+			http.Error(rw, "Creator must not be empty", 400)
+			return
+		}
+
+		if params.Tags == nil {
+			params.Tags = []string{}
+		}
+
+		newClue := Clue{
+			Id:          uuid.New(),
+			Description: params.Description,
+			Tags:        params.Tags,
+			Origin_id:   id,
+			Created_by:  params.Creator,
+			Created_at:  time.Now(),
+			Edited_by:   params.Creator,
+			Edited_at:   time.Now(),
+			Children:    []*Clue{},
+		}
+
+		tx, err := conn.Begin(r.Context())
+		if err != nil {
+			http.Error(rw, "failed to store clue", 500)
+			fmt.Printf(err.Error())
+			return
+		}
+		defer tx.Rollback(r.Context())
+
+		_, err = tx.Exec(r.Context(), `insert into clue(id, description, tags, origin_id, created_by, created_at, edited_by, edited_at) values ($1, $2, $3, $4, $5, $6, $7, $8)`,
+			newClue.Id,
+			newClue.Description,
+			newClue.Tags,
+			newClue.Origin_id,
+			newClue.Created_by,
+			newClue.Created_at,
+			newClue.Edited_by,
+			newClue.Edited_at,
+		)
+		if err != nil {
+			http.Error(rw, "failed to store clue", 500)
+			fmt.Printf(err.Error())
+			return
+		}
+
+		var cr *ClueRelation = nil
+
+		fmt.Printf("%v\n%s\n", params, body)
+
+		if params.ParentClueId != nil {
+			cr = &ClueRelation{
+				Id:        uuid.New(),
+				Parent_id: *params.ParentClueId,
+				Child_id:  newClue.Id,
+			}
+
+			// TODO validate parent exists on same sheet
+
+			_, err = tx.Exec(r.Context(), `insert into clue_relation(id, parent_id, child_id) values ($1, $2, $3)`, cr.Id, cr.Parent_id, cr.Child_id)
+			if err != nil {
+				http.Error(rw, "failed to store clue parent", 500)
+				fmt.Printf(err.Error())
+				return
+			}
+		}
+
+		err = tx.Commit(r.Context())
+		if err != nil {
+			http.Error(rw, "failed to store clue with parent", 500)
+			fmt.Printf(err.Error())
+			return
+		}
+
+		data, err := json.Marshal(struct {
+			Clue         Clue
+			ClueRelation *ClueRelation `json:,omitempty,omitzero`
+		}{Clue: newClue, ClueRelation: cr})
+		if err != nil {
+			http.Error(rw, "Failed to marshal data", 500)
+			fmt.Printf(err.Error())
+			return
+		}
+
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write(data)
+	})
+
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
